bb0c3013042531a83efd4945352e6d15e8352348
theFibonacciEffect <caspargutsche1@gmail.com>
2021-04-04 19:40:23 +0200

    fix right hand side

--- n body problem.py
+++ n body problem.py
@@ -10,11 +10,11 @@ from scipy.optimize import fsolve
 def impl_mpr(T,N, sys, ic):
     t, dt = np.linspace(0,T, N, retstep=True)
     sol = np.empty((N,) + np.shape(ic))
-    sol[0] = ic
+    sol[0,...] = ic
     for i in range(N-1):
+        print(f"sol[i] shape: {sol[i].shape}", f"sys shape:{sys(sol[0]).shape}")
         sol[i+1] = fsolve(
-            lambda x : x - (sol[i] + dt*(sys(0.5*(sol[i] + x)))), sol[i]
-            )
+            lambda x : x - (sol[i] + dt*(sys(0.5*(sol[i] + x)))), sol[i])
     return t, sol
 # %%
 N = 2
@@ -22,11 +22,13 @@ dim = 2
 G = 1
 # [planet, mass(0) or momentum (1) or position (2)]
 ic = np.empty((N,3,dim))
-m1 = 1
-m2 = 2
-p1 = p2 = [3,4]
-pos1 = np.array([5,6])
-pos2 = np.array([7,8])
+m1 = 500
+m2 = 1
+p1 = [0,0]
+p2 = [0,np.sqrt(G*m1*0.5)]
+pos1 = np.array([0,0])
+pos2 = np.array([2,0])
+# the internet says that initializing an np.array with objects doesnt result in computational speedups
 ic = np.array([
     [[m1]*dim, p1, pos1],
     [[m2]*dim, p2, pos2]
@@ -36,47 +38,34 @@ ic = np.array([
 # %%
 def rhs(state):
     "right hand side"
-    p_dot = G*np.sum(
-        get_sum(state), axis=1
-    )
-    q_dot = ic[:,2] / ic[:, 0]
+    p_dot = G*get_sum(state)
+    q_dot = state[:,2] / state[:, 0]
+    out = np.array([state[:,0], p_dot, q_dot]) # (variable, body, dimension)
+    out = np.transpose(out, (1,0,2)) # (body, variable, dimension)
+    assert out.shape == state.shape, f"shape in {state.shape} != shape out {out.shape}"
+    return out
 # %%
 def get_sum(state):
     "gets the sum for computing p_dot, satisfying the condition that i!=j"
-    masses = state[:, 0, :]
-    qs = state[:, 2, :]
-    masses, ms_T = permute(masses)
-    qs, qs_T = permute(qs)
-    m_product = masses*ms_T
-    qs_diff = qs - qs_T
-    qs_norm = np.linalg.norm(qs_diff, axis=2)
-    print(qs_norm)
-    out = m_product * qs_diff / qs_norm**3
-    print(out)
-    return mask(out)
+    n = np.shape(state)[0]
+    sum = np.zeros((n, dim))
+    for k in range(n):
+        mk = state[k,0]
+        qk = state[k, 2]
+        for i in range(n):
+            if k == i:
+                continue
+            mi = state[i,0]
+            qi = state[i,2]
+            sum[k,:] += mi*mk/(np.linalg.norm(qi - qk)**3) * (qi - qk)
+    # assert all(sum != 0)
+    print(sum)
+    return sum
 
-def permute(data):
-    """input with 2 axis
-    computes the transpose of the repeated input containing vectors
-    """
-    # print(f"data[0]: {data[0]}")
-    data = np.repeat(data[:,np.newaxis,:], data.shape[0], 1)
-    # print(f"data[0,0]: {data[0,0]}")
-    # print(f"data[1,0]: {data[1,0]}")
-    data_transp = np.swapaxes(data, 0, 1)
-    # print(f"data_transp[0,1]: {data_transp[0,1]}")
-    return data, data_transp
-    
-def mask(inp):
-    assert inp.shape[0] == inp.shape[1]
-    n = inp.shape[0]
-    dims = inp.shape[2]
-    id_ = np.identity(n, bool)
-    mask = np.tile(id_, (1,1,dims))
-    mask = np.repeat( np.identity(n, bool)[..., np.newaxis], dims, axis= -1 )
-    return np.ma.MaskedArray(inp, mask)
+# %%
+t, y = impl_mpr(3, 100, rhs, ic)
 
-# %% 
-a = permute(ic[:,2])
-get_sum(ic)
+plt.plot(*y[0,2])
+plt.plot(*y[1,2])
+plt.show()
 # %%
